efe71926fdb5a03262286dbb9480241d1802af3a
Jerin Peter George <jerinpetergeorge@gmail.com>
2024-01-11 18:49:37 +0530

    clean test models

--- tests/polls/models.py
+++ tests/polls/models.py
@@ -8,24 +8,3 @@ class JSONArrayFieldModel(models.Model):
 
     def __str__(self):
         return str(self.list_field)
-
-
-# class JSONArrayWithDefault(models.Model):
-#     list_field = JSONArrayField(default=list)
-#
-#     def __str__(self):
-#         return str(self.list_field)
-#
-#
-# class JSONArrayBlank(models.Model):
-#     list_field = JSONArrayField(blank=True)
-#
-#     def __str__(self):
-#         return str(self.list_field)
-#
-#
-# class JSONArrayNull(models.Model):
-#     list_field = JSONArrayField(null=True)
-#
-#     def __str__(self):
-#         return str(self.list_field)
